ee8d69df213cfe2d70d07137e4d55bf59c307073
Terry <terryakishin0814@gmail.com>
2022-06-30 22:31:24 +0900

    git error. so remove .git and init

---
+++ src/main/java/com/pogoweb/demo/web/WebRestController.java
@@ -0,0 +1,32 @@
+package com.pogoweb.demo.web;
+
+import com.pogoweb.demo.domain.posts.PostsRepository;
+import com.pogoweb.demo.dto.posts.PostsSaveRequestDto;
+import com.pogoweb.demo.service.PostsService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private PostsRepository postsRepository;
+    private PostsService postsService;
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+
+//    @PostMapping("/posts")
+//    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+//        postsRepository.save(dto.toEntity());
+//    }
+    @PostMapping("/posts")
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+        return postsService.save(dto);
+    }
+
+}
---
+++ src/test/java/com/pogoweb/demo/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,60 @@
+package com.pogoweb.demo.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup()
+    {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기()
+    {
+        postsRepository.save(Posts.builder().title("테스트 게시글").content("테스트 본문").author("terry@gmail.com").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("테스트 게시글"));
+        assertThat(posts.getContent(), is("테스트 본문"));
+    }
+
+    @Test
+    public void BaseTimeEntity_등록()
+    {
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("terry@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertTrue(posts.getCreatedData().isAfter(now));
+        assertTrue(posts.getModifiedData().isAfter(now));
+    }
+}
