30acba92c69fa65d75897f3ab36f7bdbdced13aa
Terry <terryakishin0814@gmail.com>
2022-07-03 17:36:27 +0900

    not found command

--- src/main/java/com/pogoweb/demo/DemoApplication.java
+++ src/main/java/com/pogoweb/demo/DemoApplication.java
@@ -9,17 +9,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class DemoApplication {
 
-//	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-//			+ "classpath:application.yml,"
-//			+ "/home/ec2-user/app/config/springboot-webservice/real-application.yml";
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/home/ec2-user/app/config/springboot-webservice/real-application.yml";
 
 //	public static void main(String[] args) {
-//		new SpringApplicationBuilder(DemoApplication.class)
-//				.properties(APPLICATION_LOCATIONS)
-//				.run(args);
+//		SpringApplication.run(DemoApplication.class, args);
 //	}
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(DemoApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+	}
 }
