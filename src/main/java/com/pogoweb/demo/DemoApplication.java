3b56d22fc5056420b2e69e6bbaf7373ee7ba787b
Terry <terryakishin0814@gmail.com>
2022-07-04 15:36:52 +0900

    not found command

--- src/main/java/com/pogoweb/demo/DemoApplication.java
+++ src/main/java/com/pogoweb/demo/DemoApplication.java
@@ -11,8 +11,8 @@ public class DemoApplication {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.yml,"
-//			+ "/home/ec2-user/app/config/springboot-webservice/real-application.yml";
-			+ "/Users/terryakishin/pogoweb/app/config/springboot-webservice/real-application.yml";
+			+ "/home/ec2-user/app/config/springboot-webservice/real-application.yml";
+
 //	public static void main(String[] args) {
 //		SpringApplication.run(DemoApplication.class, args);
 //	}
